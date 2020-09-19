e09caaa9e74fb8b03e082c7cd197c3864d04367f
Nagnogur <trostinetsky.sasha1@gmail.com>
2020-09-19 16:44:47 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+function addRow() {
+
+    var table = document.getElementById("table1");
+ 
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+ 
+    row.insertCell(0).innerHTML= '<div type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+ 
+}
+ 
+function deleteRow(obj) {
+      
+    var index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTableData");
+    table.deleteRow(index);
+    
+}
+ 
+function addTable() {
+      
+    var myTableDiv = document.getElementById("myDynamicTable");
+      
+    var table = document.createElement('TABLE');
+    table.border='1';
+    
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+      
+    for (var i=0; i<3; i++){
+       var tr = document.createElement('TR');
+       tableBody.appendChild(tr);
+       
+       for (var j=0; j<4; j++){
+           var td = document.createElement('TD');
+           td.width='75';
+           td.appendChild(document.createTextNode("Cell " + i + "," + j));
+           tr.appendChild(td);
+       }
+    }
+    myTableDiv.appendChild(table);
+    
+}
+ 
+function load() {
+    
+    console.log("Page load finished");
+ 
+}
