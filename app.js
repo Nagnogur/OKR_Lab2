5a0a8a32724244ecf024ceb326304b022de69b23
Nagnogur <trostinetsky.sasha1@gmail.com>
2020-09-19 20:24:20 +0300

    Added textarea and add note button

--- app.js
+++ app.js
@@ -1,51 +1,20 @@
+const bu = document.getElementById("button1");
+var n = 0;
 
 
 function addRow() {
 
-    var table = document.getElementById("table1");
- 
-    var rowCount = table.rows.length;
-    var row = table.insertRow(rowCount);
- 
-    row.insertCell(0).innerHTML= '<div type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
- 
-}
- 
-function deleteRow(obj) {
-      
-    var index = obj.parentNode.parentNode.rowIndex;
-    var table = document.getElementById("myTableData");
-    table.deleteRow(index);
-    
-}
- 
-function addTable() {
-      
-    var myTableDiv = document.getElementById("myDynamicTable");
-      
-    var table = document.createElement('TABLE');
-    table.border='1';
-    
-    var tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
-      
-    for (var i=0; i<3; i++){
-       var tr = document.createElement('TR');
-       tableBody.appendChild(tr);
-       
-       for (var j=0; j<4; j++){
-           var td = document.createElement('TD');
-           td.width='75';
-           td.appendChild(document.createTextNode("Cell " + i + "," + j));
-           tr.appendChild(td);
-       }
-    }
-    myTableDiv.appendChild(table);
-    
-}
- 
-function load() {
-    
-    console.log("Page load finished");
- 
+    var tableRef = document.getElementById('table1').getElementsByTagName('tbody')[0];
+
+// Insert a row in the table at the last row
+var newRow   = tableRef.insertRow();
+
+// Insert a cell in the row at index 0
+var newCell  = newRow.insertCell(0);
+
+// Append a text node to the cell
+var newButton  = document.createElement("button");
+newButton.innerHTML = "<h1>New note" + n + "</h1>";
+n++;
+newCell.appendChild(newButton);
 }
