9a4d7c7e6e8f280c342134c734e771d2375a2ead
Nagnogur <trostinetsky.sasha1@gmail.com>
2020-09-23 17:35:04 +0300

    Added selection

--- app.js
+++ app.js
@@ -1,146 +1,137 @@
-/*const bu = document.getElementById("button1");
-var n = 0;
-var notes = new Array();
-var buttons = document.getElementById("table1").getElementsByTagName("button");
-
-function addRow() {
-    for (const element of buttons) {
-        element.style = "background-color: rgb(29, 28, 35, 30);";
-    }
-    var tableRef = document.getElementById('table1').getElementsByTagName('tbody')[0];
-    var newRow   = tableRef.insertRow(0);
-    var newCell  = newRow.insertCell(0);
-    var newButton  = document.createElement("button");
-    var today = new Date();
-    var date = today.getDate() + '.' + (today.getMonth()+1) + '.' + today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    newButton.textContent = "New note" + " " + n;
-    newButton.id = "note" + n;
-    var note = {
-        "id" : newButton.id,
-        "time" : date + " " + time
-    };
-    notes.unshift(note);
-    n++;
-    newCell.appendChild(newButton);
-    Select(newButton.id);
-}
-
-function Select(id){
-    for (const element of buttons) {
-        element.style = "background-color: rgb(29, 28, 35, 30);";
-      }
-    var selectedButton = document.getElementById(id);
-    selectedButton.style = "background-color: rgb(201, 149, 33, 110);";
-}
-
-document.addEventListener('click',function(e){
-    console.log(e.target + ' - ' + e.target.id);
-    if(e.target.id.slice(0, 4) == 'note'){
-        console.log(111);
-          Select(e.target.id);
-     }
- });*/
+window.addEventListener("hashchange", showNotes, false);
 
 showNotes();
 
 var selected;
 
- function showNotes() {
-    var notes = localStorage.getItem("notes");
-    if (notes == null) {
-      notesObj = [];
-    } else {
-      notesObj = JSON.parse(notes);
+function showNotes() {
+  var notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } 
+  else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    var cuttedText;
+    if (element.text.length > 26){
+      cuttedText = element.text.slice(0, 24) + "...";
     }
-    let html = "";
-    notesObj.forEach(function(element, index) {
-      var cuttedText;
-      if (element.text.length > 26){
-        cuttedText = element.text.slice(0, 24) + "...";
-      }
-      else{
-        cuttedText = element.text;
-      }
-      html += `<div class="noteCard col-md-9 card">
-                  <div class="card-body" onclick="Select(this.id)">
-                    <h5 class="card-title" style="line-height: 80%; margin-top: -5%;">${element.title}</h5>
+    else{
+      cuttedText = element.text;
+    }
+    var cuttedName;
+    if (element.title.length > 16){
+      cuttedName = element.title.slice(0, 16) + "...";
+    }
+    else{
+      cuttedName = element.title;
+    }
+    html += `<div class="row">
+                <div class="noteCard col-md-9 card">
+                  <div class="card-body" onclick="Select(this.id)" id="${index}">
+                    <h5 class="card-title" style="line-height: 80%; margin-top: -5%;">${cuttedName}</h5>
                     <p class="card-date" > ${element.date} </p>
                     <p class="card-text" style="margin-top: -10%; color: rgb(179, 179, 179);"> <small> ${cuttedText} </small> </p>
                   </div>
                 </div>
-  <div class="delBtn col-md-3">
-      <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary"> Delete </button>
-  </div>`;
-    });
-    var notesElm = document.getElementById("notes");
-      notesElm.innerHTML = html;
+                <button id="${index}"onclick="deleteNote(this.id)" class="btn col-md-3"> Delete </button>
+              </div>`;
+  });
+  var notesElm = document.getElementById("notes");
+  notesElm.innerHTML = html;
+  var addTxt = document.getElementById("textarea1");
+  var addTitle = document.getElementById("noteName");
+  var ref = location.hash;
+  if (ref == ""){
+    addTxt.value = "";
+    addTitle.value = "";
+  }
+  else{
+    ref = ref.slice(1, ref.length);
+    var refNote = notesObj.filter(obj => {return obj.ref === ref});
+    console.log(refNote);
+    addTitle.value = refNote[0].title;
+    addTxt.value = refNote[0].text;
+  }
 };
 
 function Select(index){
-  if (!confirm("Are you sure?")){
-    return;
-  }
   var notes = localStorage.getItem("notes");
-  
+  var addTxt = document.getElementById("textarea1");
+  var addTitle = document.getElementById("noteName");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-
-  notesObj.splice(index, 1);
+  location.hash = notesObj[index].ref;
+  addTitle.value = notesObj[index].title;
+  addTxt.value = notesObj[index].text;
   localStorage.setItem("notes", JSON.stringify(notesObj));
-  showNotes();
+}
+
+function AddZero(time){
+  if (time < 10){
+    return "0" + time;
+  }
+  else return time;
 }
 
 var addBtn = document.getElementById("button1");
 addBtn.addEventListener("click", function(e) {
+  if (location.hash != ""){
+    return;
+  }
   var addTxt = document.getElementById("textarea1");
   var addTitle = document.getElementById("noteName");
   var today = new Date();
-  var date = today.getDate() + '.' + (today.getMonth()+1) + '.' + today.getFullYear();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var date = AddZero(today.getDate()) + '.' + AddZero(today.getMonth() + 1) + '.' + today.getFullYear();
+  var time = AddZero(today.getHours()) + ":" + AddZero(today.getMinutes()) + ":" + AddZero(today.getSeconds());
   if (addTitle.value == "")
   {
-   
     alert("Please enter note name");
     return;
   }
   var notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
-  } else {
+  } 
+  else {
     notesObj = JSON.parse(notes);
   }
+  var hash = addTitle.value;
   var myObj = {
     title: addTitle.value,
     text: addTxt.value,
-    date: date + " " + time
+    date: date + " " + time,
+    ref: hash
   }
   notesObj.unshift(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
-  addTitle.value = "";
+  /*addTxt.value = "";
+  addTitle.value = "";*/
+
+  location.hash = hash;
+
 //   console.log(notesObj);
   showNotes();
 });
 
 function deleteNote(index) {
-      
-      if (!confirm("Are you sure?")){
-        return;
-      }
-      var notes = localStorage.getItem("notes");
-      
-      if (notes == null) {
-        notesObj = [];
-      } else {
-        notesObj = JSON.parse(notes);
-      }
-    
-      notesObj.splice(index, 1);
-      localStorage.setItem("notes", JSON.stringify(notesObj));
-      showNotes();
-    }
+  if (!confirm("Are you sure?")){
+    return;
+  }
+  var notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } 
+  else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
     
